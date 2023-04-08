99a651adc27ea1f124cfbf337f71eeea630b7d60
souravsarker2015 <souravsarker2015@gmail.com>
2023-04-09 01:20:52 +0600

    header footer added from flowbite tailwind css

--- cinemate/src/App.js
+++ cinemate/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
 import {AllRoutes} from "./route/AllRoutes";
+import {Footer, Header} from "./components";
 
 function App() {
     return (
         <div className="App">
+            <Header/>
             <AllRoutes/>
+            <Footer/>
         </div>
     );
 }
