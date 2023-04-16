2f850a615a2c5d9f5e0ffd7b17259efeb2a3b781
souravsarker2015 <souravsarker2015@gmail.com>
2023-04-17 01:45:55 +0600

    scroll to tp implemented

--- cinemate/src/components/index.js
+++ cinemate/src/components/index.js
@@ -2,3 +2,4 @@ export {Card} from "./Card";
 export {Header} from "./Header";
 export {Footer} from "./Footer";
 export {Button} from "./Button";
+export {ScrollToTop} from "./ScrollToTop";
--- cinemate/src/index.js
+++ cinemate/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 
 import {BrowserRouter as Router} from "react-router-dom";
+import {ScrollToTop} from "./components";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Router>
+            <ScrollToTop/>
             <App/>
         </Router>
 
